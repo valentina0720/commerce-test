7a44ff7c7be9bf49e6405f16718c7158ac6b6c91
valentina0720 <110473675+valentina0720@users.noreply.github.com>
2022-08-13 13:54:03 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,22 @@
+const enter = document.getElementById("enter");
+
+const email = document.getElementById("email");
+
+const password = document.getElementById("password");
+
+const eAlert = document.getElementById("emailAlert");
+
+const pAlert = document.getElementById("passwordAlert");
+
+function validar() {
+    if (email.value.length == 0) {
+        eAlert.innerHTML = "Ingresa tu email";
+    }
+    if (password.value.length < 6) {
+        pAlert.innerHTML = ("Ingresa tu contraseña");
+    }
+    else {document.formulario.submit();
+        window.location.replace("index.html" )}
+}
+
+enter.addEventListener("click", validar);
